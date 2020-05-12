d733a2e7c3c425a28a0725cc3630adaed5db8e42
aniraj1688 <58540303+aniraj1688@users.noreply.github.com>
2020-05-12 20:00:40 +0530

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,11 +1,8 @@
 const cvs=document.getElementById('canvas');
 const ctx=cvs.getContext('2d');
-let foodimg=new Image();
-foodimg.src="frog.png";
 ctx.fillStyle="red";
 ctx.fillRect(100,100,40,40);
-
-
-
+let foodimg=new Image();
+foodimg.src="frog.png";
 ctx.drawImage(foodimg,0,0,50,50);
 
